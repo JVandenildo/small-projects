e3460f64adf511629ec0c227eae88b921337b2cd
J. Vandenildo <jvandenildo@gmail.com>
2023-08-07 19:18:43 -0300

    button function rotate

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,12 +1,16 @@
 const button = document.querySelector("#button");
+const disc = document.querySelector("#disc");
 button.addEventListener("click", test);
 
 function test() {
-	if (button.innerHTML === "Tested!") {
-		button.innerHTML = `Rotate!`;
-	} else {
-		button.innerHTML = `Tested!`;
-	}
+	if (button.innerHTML === "Rotate") {
+		button.innerHTML = `Pause`;
+		disc.style.cssText = "transform: rotate(180deg)";
 
-	return true;
+		return true;
+	} else {
+		button.innerHTML = `Rotate`;
+
+		return false;
+	}
 }
