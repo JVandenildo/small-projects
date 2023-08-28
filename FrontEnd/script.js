af6d24a881370f0c28960e08ca32d549b7e817fb
J. Vandenildo <jvandenildo@gmail.com>
2023-08-28 18:35:23 -0300

    rotate, simple dark mode

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,17 +1,31 @@
-const playButton = document.querySelector("#play");
+const playButton = document.querySelector("#playButton");
 playButton.addEventListener("click", playMedia);
+const modeChecker = document.querySelector("#modeChecker");
+const playIcon = document.querySelectorAll(".playIcon");
+modeChecker.addEventListener("change", () => {
+	document.body.classList.toggle("dark");
+});
 
 function playMedia() {
 	switch (playButton.innerHTML) {
 		case "Play":
 			playButton.innerHTML = "Pause";
+			for (let i in playIcon) {
+				playIcon[i].style = `transform: rotate(180deg)`;
+			}
 
 			break;
 
 		case "Pause":
 			playButton.innerHTML = "Play";
+			for (let i in playIcon) {
+				playIcon[i].style = `transform: rotate(0deg)`;
+			}
 
 			break;
+
+		default:
+			alert("Error");
 	}
 
 	return true;
