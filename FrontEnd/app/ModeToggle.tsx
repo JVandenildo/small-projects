0e6de93d737676805c0b3cc9ef29dc3768bf13fc
J. Vandenildo <jvandenildo@gmail.com>
2023-09-01 14:32:17 -0300

    react first codes w. next

---
+++ FrontEnd/app/ModeToggle.tsx
@@ -0,0 +1,10 @@
+// toggle dark-light mode on a page
+
+export default function ModeToggle() {
+	return (
+		<>
+			<input type="checkbox" name="modeToggler" id="modeToggler" />
+			<img src="../public/images/circle.png" alt="circle" />
+		</>
+	);
+}
---
+++ FrontEnd/app/page.tsx
@@ -0,0 +1,11 @@
+import ClassesNRaces from "./ClassesNRaces";
+import ModeToggle from "./ModeToggle";
+
+export default function Page() {
+	return (
+		<>
+			<ModeToggle />
+			<ClassesNRaces />
+		</>
+	);
+}
