2b9bb4373973ba9987b5b897e5cf8425ed8dafd9
J. Vandenildo <jvandenildo@gmail.com>
2024-02-22 18:40:19 -0300

    minor changes

--- priorityOrder.py
+++ priorityOrder.py
@@ -3,17 +3,20 @@
 import random as rd
 
 
-def priorityOrder(array: list[int], place: int):
+def priorityOrder(array: list[int], number: int):
     copiedArray = array.copy()
     count = 1
 
-    while len(copiedArray) > 1 and max(copiedArray) != place:
+    while len(copiedArray) > 1 and max(copiedArray) != number:
         copiedArray.remove(max(copiedArray))
         count = count + 1
 
     return count
 
 
-lst = [rd.randint(1, 100) for i in range(rd.randint(5, 7))]
+# just for demonstration
+lst = [rd.randint(1, 100) for i in range(rd.randint(5, 10))]
 lst.append(50)
 print(f"List: {lst}\n{priorityOrder(lst, 50)}")
+lst.sort()
+print(lst)
