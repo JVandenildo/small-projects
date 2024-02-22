00608153481a9b8f5f881e0aca35be123c2e7511
J. Vandenildo <jvandenildo@gmail.com>
2024-02-22 19:06:42 -0300

    int to float data types

--- Areas/SphereArea.py
+++ Areas/SphereArea.py
@@ -5,5 +5,5 @@ def SphereArea(radius):
     return round(4 * pi * (pow(radius, 2)), 2)
 
 
-radius = int(input("Radius: "))
+radius = float(input("Radius: "))
 print(f"Area of the sphere's surface is {SphereArea(radius)}.")
