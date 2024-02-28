b81e62149ad94914a6b1a0d99635b7574f1136c0
J. Vandenildo <jvandenildo@gmail.com>
2024-02-28 14:43:29 -0300

    commentary

--- priorityOrder.py
+++ priorityOrder.py
@@ -1,9 +1,18 @@
-# this function is for finding how big is a number on a list
-
-import random as rd
+import random as rd  # generate a random list, for testing
 
 
-def priorityOrder(array: list[int], number: int):
+def priorityOrder(array: list[int], number: int) -> int:
+    """
+    Returns how big is the number within the list.
+
+    Args:
+        array: is the list the function will examine.
+        number: the number that will compare to the other numbers within the list.
+
+    Returns:
+        An int representing of hoe big is the number. 1 for the biggest, 2 for the 2nd biggest, so forth.
+    """
+
     copiedArray = array.copy()
     count = 1
 
@@ -14,9 +23,9 @@ def priorityOrder(array: list[int], number: int):
     return count
 
 
-# just for demonstration
+# just for testing
 lst = [rd.randint(1, 100) for i in range(rd.randint(5, 10))]
-lst.append(50)
+lst.append(50)  # a number I know it will be there
 print(f"List: {lst}\n{priorityOrder(lst, 50)}")
 lst.sort()
 print(lst)
